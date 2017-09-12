80014682af549816fdfa68c5ff23104ff16a1cc4
Evan Radford <Eradford45@gmail.com>
2017-09-12 16:09:15 -0600

    model test works

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   describe 'validations' do
+      it { should validate_presence_of(:eye_color) }
+      it { should validate_presence_of(:hair_color) }
+    end
 end
